e32df1d8a4814fd4c7de4e071d822935ff638426
mvp-rae <rae@mvpfastlane.com>
2018-09-17 21:36:18 +0800

    environment setting done

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Config from 'react-native-config';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,6 +25,9 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text>
+          {Config.ENV}
+        </Text>
       </View>
     );
   }
